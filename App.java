457504b840f19d7bde01f773774efdf3675923cc
YeicolCR02 <yeicol2001s@gmail.com>
2025-09-27 18:21:25 -0600

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Prueba!");
+        System.out.println("Hello, Prueba 2");
     }
 }
