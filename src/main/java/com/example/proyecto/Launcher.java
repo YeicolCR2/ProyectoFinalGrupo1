2752de5eb228a059e8ec1b3835cb70ffd90ef3a9
YeicolCR2 <yeicol2001s@gmail.com>
2025-10-05 13:11:30 -0600

    modifique el launcher

--- src/main/java/com/example/proyecto/Launcher.java
+++ src/main/java/com/example/proyecto/Launcher.java
@@ -3,9 +3,6 @@ package com.example.proyecto;
 import javafx.application.Application;
 
 public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-
-
+    public static void main(String[] args) { Application.launch(HelloApplication.class, args);
     }
 }
